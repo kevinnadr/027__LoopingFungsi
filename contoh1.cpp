0e024139f4e5531405f1c19c43c70bd475cb9c9e
kevinnadr <kevin19305.ib@gmail.com>
2024-03-19 19:31:32 +0700

    commit contoh ke2

---
+++ contoh1.cpp
@@ -0,0 +1,25 @@
+//contoh pertama
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    string nama[5];
+
+    for (i = 1; i > 10; i+=10)
+    {
+        cout << i << ". " << "SELAMAT PUASA" << endl;
+    }
+    cout << "Nilai i terakhir = " << i << endl;
+
+    for (i = 0; i < 5; i++){
+        cout << "Masukkan nama ke-" << i << ":";
+        cin >> nama[i];
+    }
+
+    for (i = 0; i < 5; i++){
+        cout << "Nama ke-" << i << ": " << nama[i] << endl;
+    }
+}
